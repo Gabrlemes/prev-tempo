c195b12dfd7f02abd8fb9d09dd28d4e4d9674e25
Gabriel lemes <gabrielsiber816@gmail.com>
2024-04-18 22:08:15 -0300

    finalizado

--- scripts.js
+++ scripts.js
@@ -1,25 +1,26 @@
 
-const key = "6d94142a10d9c0942c576dadf8e40903"
+    let key = "6d94142a10d9c0942c576dadf8e40903" 
 
-function colocarDadosTela(dados) {
-    console.log(dados)
-    document.querySelector(".cidade").innerHTML = "tempo em " + dados.name
-    document.querySelector(".tempo").innerHTML = Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".texto-clima").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
-    document.querySelector(".img").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
-}
-
-
-async function buscarCidade(cidade){
-
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
-
-    colocarDadosTela(dados)
-}
+    function colocarDadosTela(dados) {
+        
+        console.log(dados)
+        document.querySelector(".cidade").innerHTML = "tempo em " + dados.name
+        document.querySelector(".tempo").innerHTML = Math.floor(dados.main.temp) + "°C"
+        document.querySelector(".texto-clima").innerHTML = dados.weather[0].description
+        document.querySelector(".umidade").innerHTML = "umidade" + dados.main.humidity + "%"
+        document.querySelector(".img").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    }
 
 
-function clickOnButton(){
-    const cidade = document.querySelector(".input-cidade").value
-    buscarCidade(cidade)
-}
+    async function buscarCidade(cidade) {
+
+        let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + key + "&lang=pt_br" + "&units=metric").then(resposta => resposta.json())
+                                
+        colocarDadosTela(dados)
+    }
+
+
+    function clickOnButton() {
+        let cidade = document.querySelector(".input-cidade").value
+        buscarCidade(cidade)
+    }
